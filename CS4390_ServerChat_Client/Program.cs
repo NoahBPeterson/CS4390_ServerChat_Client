4db2dd50658e517ff721e123631fadbe599407fa
Noah Peterson <nbp160130@utdallas.edu>
2019-07-22 18:02:11 -0500

    Adding challenge response

--- CS4390_ServerChat_Client/Program.cs
+++ CS4390_ServerChat_Client/Program.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Net;
+using System.Text;
 
 namespace CS4390_ServerChat_Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //If you run this with the argument '192.168.1.2', it'll use that as the server address to connect to.
         {
             Console.WriteLine("Welcome to the Group 2 Chat Client!");
             Console.WriteLine("\n Enter your client ID.");
             string clientID = Console.ReadLine();
+            Console.WriteLine("\n Enter your client password.");
+            string password = Console.ReadLine();
             string serverIPPort = "";
-            if(args.Length==0) //If the console has not accepted any arguments, ask for the IP and port.
+            if(args.Length==0) //If the console has not accepted any arguments, ask for the IP.
             {
                 Console.WriteLine("\n Please enter the IP address of the server. (Example: 1.1.1.1)");
                 serverIPPort = Console.ReadLine();
@@ -19,16 +22,31 @@ namespace CS4390_ServerChat_Client
             {
                 serverIPPort = args[0];
             }
-            UDPConnection udpConnection = new UDPConnection(clientID, serverIPPort);
+            UDPConnection udpConnection = new UDPConnection(clientID, password, serverIPPort);
 
 
-            IPEndPoint response = null;
+            string response = null; //When not null, contains rand_cookie and tcpPortNumber
             while (response==null)
             {
                 response = udpConnection.UDPConnect();
             }
-            Console.WriteLine("Finished?: "+response.ToString());
-                while(true)
+            string cookie = "";
+            string port = "";
+            int space = 0;
+            for(int i = 0; i < response.Length; i++)
+            {
+                if (response[i] == ' ')
+                {
+                    space = i;
+                }
+                cookie = response.Substring(0, i);
+            }
+            byte[] cookieNum = Encoding.ASCII.GetBytes(cookie);
+            port = response.Substring(space, response.Length - space);
+            byte[] portNum = Encoding.ASCII.GetBytes(port);
+            Console.WriteLine("Finished?: "+cookie+" "+port);
+            Console.WriteLine(cookieNum + " " + portNum);
+            while(true)
             {
 
             }
--- CS4390_ServerChat_Client/UDPConnection.cs
+++ CS4390_ServerChat_Client/UDPConnection.cs
@@ -1,38 +1,52 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace CS4390_ServerChat_Client
 {
     public class UDPConnection
     {
-        string clientID;
+        string clientID, privateKey;
         IPEndPoint serverAddress;
+        IPEndPoint clientAddress;
         Socket udpConnectionSocket;
-	    public UDPConnection(string clientID, string serverIP)
+	    public UDPConnection(string clientID, string clientPass, string serverIP)
 	    {
             this.clientID = clientID;
+            privateKey = clientPass;
             serverAddress = new IPEndPoint(IPAddress.Parse(serverIP), 10020);
-            udpConnectionSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            udpConnectionSocket.Connect(serverAddress);
+            clientAddress = new IPEndPoint(IPAddress.Any, 0); //Our address
+            udpConnectionSocket = new Socket(clientAddress.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+            udpConnectionSocket.Bind(clientAddress);
         }
 
 
-        public IPEndPoint UDPConnect()
+        public string UDPConnect()
         {
-            //while(clientID=="noahb")
-            { 
-                UDPSend(clientID); // HELLO
-                //Console.WriteLine("Sent another Hello!");
-            }
-            int tcpPort = UDPReceive();
-            if(tcpPort<5)
-            {
-                return null;
-            }
-            IPEndPoint tcpHostEndPoint = new IPEndPoint(serverAddress.Address, tcpPort);
-            return tcpHostEndPoint;
+            UDPSend(clientID); // HELLO
+            string response = UDPReceive(); //Receive challenge (random integer, challenge)
+
+            int serverChallenge = 0;
+            int.TryParse(response, out serverChallenge); //Convert challenge to integer
+            string clientResponse = Encoding.ASCII.GetString(challengeReponse(serverChallenge)); //Get response, convert from byte[] to string
+
+            UDPSend(clientID +" "+ clientResponse); //Send clientID + challenge Response
+
+            string tcp = UDPReceive(); //Receive rand_cookie, port_number for TCP connection
+
+            udpConnectionSocket.Close();  //Close socket when done.
+            return tcp;
+        }
+
+        public byte[] challengeReponse(int rand)
+        {
+            SHA256 encryptionObject = SHA256.Create();
+            byte[] randKey = Encoding.ASCII.GetBytes(rand.ToString() + privateKey);
+            byte[] randHash = encryptionObject.ComputeHash(randKey); //Hashes challenge with out privateKey (password)
+            
+            return randHash;
         }
 
         //Call this function with string you wish to send to the server (Client ID, challenge response, etc.
@@ -41,13 +55,11 @@ namespace CS4390_ServerChat_Client
         {
             try
             {
-
-
                 //Send message to server using UDP.
                 byte[] buffer = Encoding.ASCII.GetBytes(sendText);
-                //udpConnectionSocket.SendToAsync( //(buffer, SocketFlags.None);
-                udpConnectionSocket.SendTo(buffer, buffer.Length, SocketFlags.None, serverAddress);
-                udpConnectionSocket.SendTo(buffer, serverAddress);
+                EndPoint hostEndPoint = (EndPoint)serverAddress;
+                udpConnectionSocket.SendTo(buffer, buffer.Length, SocketFlags.None, hostEndPoint);
+                //udpConnectionSocket.SendTo(buffer, hostEndPoint);
 
             }
             catch (SocketException e) { }
@@ -55,7 +67,7 @@ namespace CS4390_ServerChat_Client
             catch (Exception e) { }
         }
 
-        public int UDPReceive()
+        public string UDPReceive()
         {
             string receiveString = "";
 
@@ -64,17 +76,18 @@ namespace CS4390_ServerChat_Client
             try
             {
                 EndPoint EP = (EndPoint)serverAddress;
+                udpConnectionSocket.ReceiveTimeout = 1200000;
                 Int32 receive = udpConnectionSocket.ReceiveFrom(receiveBytes, ref EP);
                 receiveString += Encoding.ASCII.GetString(receiveBytes); //(receive, 0, receiveBytes);
                 receiveString = receiveString.Substring(0, receive);
 
-                while (receive > 0)
+                /*while (receive > 0)
                 {
                     receive = udpConnectionSocket.ReceiveFrom(receiveBytes, receiveBytes.Length, 0, ref EP);
                     receiveString += Encoding.ASCII.GetString(receiveBytes, receiveBytes.Length, 0);
-                }
-
-                int tcpPort;
+                }*/
+                return receiveString;
+                /*int tcpPort;
 
                 if (int.TryParse(receiveString, out tcpPort) == true)
                 {
@@ -83,14 +96,11 @@ namespace CS4390_ServerChat_Client
                 else
                 {
                     Console.WriteLine("DEBUG: Received string from server: " + receiveString);
-                }
+                }*/
             }
             catch (Exception e) { }
 
-
-            udpConnectionSocket.Close();  //Close socket when done.
-
-            return 0; //Return TCP port number
+            return "";
 
         }
     }
