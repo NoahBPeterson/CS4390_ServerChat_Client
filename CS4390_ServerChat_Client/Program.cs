6d8ac53fca91bf0027725d6e41a1d8fa269049a6
Stephanie Quinto <stephanieq.97@gmail.com>
2019-07-30 18:58:13 -0500

    Add files via upload
    Hey this is the encrypting and decrypting script all it needs is for us to integrate it to the program we are currently doing. I am not 100% sure who all is working on it or if it has been pushed so please integrate it!! Thank you! :)

--- CS4390_ServerChat_Client/Program.cs
+++ CS4390_ServerChat_Client/Program.cs
@@ -1,46 +1,89 @@
-﻿using System;
-using System.Net;
+﻿/*Encryption class for CS 4390, Dr. Le's class. This encryption program runs MD5 and Triple DES using a hashing algorithm per
+ * requested. This is for use in the Server Project for Team 2*/
+
+using System;
+using System.Security.Cryptography;
 using System.Text;
 
-namespace CS4390_ServerChat_Client
+namespace Encryptey
 {
     class Program
     {
-        static void Main(string[] args) //If you run this with the argument '192.168.1.2', it'll use that as the server address to connect to.
+        //we have to set a key or password as great as our professor
+        static string password { get; set; } = "ProF3sS0RLeIsAw3S0MEInCS4390";
+
+        static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Group 2 Chat Client!");
-            Console.WriteLine("\n Enter your client ID.");
-            string clientID = Console.ReadLine();
-            Console.WriteLine("\n Enter your client password.");
-            string password = Console.ReadLine();
-            string serverIP = "";
-            if(args.Length==0) //If the console has not accepted any arguments, ask for the IP.
-            {
-                Console.WriteLine("\n Please enter the IP address of the server. (Example: 1.1.1.1)");
-                serverIP = Console.ReadLine();
-            }else
-            {
-                serverIP = args[0];
-            }
-            UDPConnection udpConnection = new UDPConnection(clientID, password, serverIP);
-            string response = udpConnection.UDPConnect();
-            if (response.Equals("FAIL"))
-            {
-                Console.ReadKey();
-                Main(args);
-            }
-            int[] cookie_port = UDPConnection.udpParse(response);
-            TCPConnection tcpConnection = new TCPConnection(cookie_port[0], new IPEndPoint(IPAddress.Parse(serverIP), cookie_port[1]));
-            tcpConnection.TCPConnect();
-            //tcpConnection.send/receive();
+            //here goes the message being sent from server to client or vice versa
+            var messageSent = "This is the message being sent";
 
-            while (true)
-            {
+            //this line can be uncommented to view the cryption in console
+           Console.WriteLine(messageSent);
 
-            }
+            //here the message will go through the encryption process
+            var encryptedMessage = Encrypt(messageSent);
 
+            //this line can be uncommented to view the cryption in console
+            Console.WriteLine(encryptedMessage);
 
+            //finally here the messsage is decrypted
+            messageSent = Decrypt(encryptedMessage);
+
+            //this line can be uncommented to view the cryption in console
+            Console.WriteLine(messageSent);
+
+            Console.ReadKey();
         }
 
+        //here is the encryption class
+        public static string Encrypt(string messageSent)
+        {
+            //this will use MD5 as well as Triple DES
+            using (var CryptoMD5 = new MD5CryptoServiceProvider())
+            {
+                using (var TripleDES = new TripleDESCryptoServiceProvider())
+                {
+                    //hashing and encryption begins here based on the password above
+                    TripleDES.Key = CryptoMD5.ComputeHash(UTF8Encoding.UTF8.GetBytes(password));
+                    TripleDES.Mode = CipherMode.ECB;
+                    TripleDES.Padding = PaddingMode.PKCS7;
+
+                    //creates an encryption from the library
+                    using (var crypt = TripleDES.CreateEncryptor())
+                    {
+                        //actual cryption goes here and accomodates for the length of the given string
+                        byte[] messageBytes = UTF8Encoding.UTF8.GetBytes(messageSent);
+                        byte[] totalBytes = crypt.TransformFinalBlock(messageBytes, 0, messageBytes.Length);
+                        return Convert.ToBase64String(totalBytes, 0, totalBytes.Length);
+                    }
+                }
+            }
+        }
+
+        //here lies the decryptor we will use for the encrypted message sent
+        public static string Decrypt(string encryptedMessage)
+        {
+            //once again this will be based on MD5 and Triple DES
+            using (var CryptoMD5 = new MD5CryptoServiceProvider())
+            {
+                //creating an instance for tripleDES
+                using (var TripleDES = new TripleDESCryptoServiceProvider())
+                {
+                    //here the password is read and cyphered
+                    TripleDES.Key = CryptoMD5.ComputeHash(UTF8Encoding.UTF8.GetBytes(password));
+                    TripleDES.Mode = CipherMode.ECB;
+                    TripleDES.Padding = PaddingMode.PKCS7;
+
+                    //here the decrytion occurs as we create an instance to begin decrypting
+                    using (var crypt = TripleDES.CreateDecryptor())
+                    {
+                        //here the magic happens as we decrypt what was sent and allow us to get the original message
+                        byte[] cipherBytes = Convert.FromBase64String(encryptedMessage);
+                        byte[] totalBytes = crypt.TransformFinalBlock(cipherBytes, 0, cipherBytes.Length);
+                        return UTF8Encoding.UTF8.GetString(totalBytes);
+                    }
+                }
+            }
+        }
     }
 }
